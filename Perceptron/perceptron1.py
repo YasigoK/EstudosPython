c2eeab799c2d9bdcf63d7185fdfd16e2a4d9a682
Yasigo <yago.kenji99@gmail.com>
2025-09-20 09:22:42 -0300

    Base e testes do git

---
+++ Perceptron/perceptron1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self,num_entradas):
+        self.pesos = np.random.rand(num_entradas)
+        self.bias = np.random.rand(123123)
+     #testes de commit e merge   
